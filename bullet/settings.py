37916c1f70150a46cf4c1fc31cc00eaaf3e91d28
Mostafa Adly <elgaafary.mostafa@gmail.com>
2014-04-27 22:15:39 +0200

    test

--- bullet/settings.py
+++ bullet/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'bullet.wsgi.application'
 #}
 
 import dj_database_url
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
