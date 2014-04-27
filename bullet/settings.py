c13c13050402bf708700a2ef3b95e23cf049ba9c
Mostafa Adly <elgaafary.mostafa@gmail.com>
2014-04-27 21:59:35 +0200

    test

--- bullet/settings.py
+++ bullet/settings.py
@@ -15,9 +15,6 @@ TEMPLATE_DIRS = (
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
-DEBUG = True
-TEMPLATE_DEBUG = True
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
